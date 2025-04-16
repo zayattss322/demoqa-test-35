8e3c999821b62f676a6843dada59dcd3759d2f16
zayattss322 <zayattss322@gmail.com>
2025-04-16 09:26:48 +0300

    do some fixes after feedback

--- src/test/java/HomeWork3Test.java
+++ src/test/java/HomeWork3Test.java
@@ -11,7 +11,6 @@ public class HomeWork3Test {
     public static String firstName = "Shadow" ;
     public static String secondName = "Fiend" ;
     public static String userMail = "sf@ya.ru" ;
-    public static String userSex = "Male" ;
     public static String phoneNumber = "4815162342" ;
     public static String imagePath = "dota-shadow-fiend.gif" ;
     public static String myFavouriteSubject = "Chemistry" ;
@@ -22,8 +21,7 @@ public class HomeWork3Test {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000 ;
-        Configuration.holdBrowserOpen = true ;
+        Configuration.holdBrowserOpen = false ;
 
     }
 
@@ -34,7 +32,7 @@ public class HomeWork3Test {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(secondName);
         $("#userEmail").setValue(userMail);
-        $(byText(userSex)).click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue(phoneNumber);
 
 
@@ -65,17 +63,16 @@ public class HomeWork3Test {
 
         // Проверка результатов
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(
-                text(firstName + " " + secondName),
-                text(userMail),
-                text(userSex),
-                text(phoneNumber),
-                text("08 May,1991"),
-                text(myFavouriteSubject),
-                text("Sports, Music"),
-                text(imagePath),
-                text(myAddress),
-                text("NCR Delhi")
-        );
+        $(".table-responsive").shouldHave(text(firstName + " " + secondName));
+        $(".table-responsive").shouldHave(text(userMail));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text(phoneNumber));
+        $(".table-responsive").shouldHave(text("08 May,1991"));
+        $(".table-responsive").shouldHave(text(myFavouriteSubject));
+        $(".table-responsive").shouldHave(text("Sports, Music"));
+        $(".table-responsive").shouldHave(text(imagePath));
+        $(".table-responsive").shouldHave(text(myAddress));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+
     }
 }
